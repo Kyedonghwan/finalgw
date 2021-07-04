fc7abeec179d7e12226c3be3596611fd643b81d6
Kyedonghwan <kdh643737@gmail.com>
2021-07-04 17:36:48 +0900

    first commit

---
+++ src/main/java/com/it/herb/FinalgwApplication.java
@@ -0,0 +1,13 @@
+package com.it.herb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalgwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalgwApplication.class, args);
+	}
+
+}
